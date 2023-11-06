f3573f30dca6f65a7b88abcf133e4c24de2f7c1c
Anushka-Karamchandani-662 <anushkakaramchandani1206@gmail.com>
2023-11-06 12:53:04 +0530

    New commit

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,14 @@
+import React from "react";
+import "./Home.css";
+import quoteImage from "../images/QuoteImage.png";
 function Home() {
-    return(
-        <div>Home page</div>
-    );
+  return (
+    <div>
+      <img src={quoteImage} alt="Quote Vocab" className="quoteImage" />
+    </div>
+  );
 }
 
 export default Home;
+//#DBF2F4
+//#00E5F4
